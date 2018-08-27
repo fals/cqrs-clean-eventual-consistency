1169a5bd7b4de49f27311f5ab818758211b910fc
Filipe Lima <fals.xp@gmail.com>
2018-08-27 20:30:18 +0200

    Update Asp.Net Core to version 2.1

---
+++ src/Ametista.IntegrationTest/CardApiTests.cs
@@ -0,0 +1,38 @@
+using Ametista.Command.Commands;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net.Http;
+
+
+namespace Ametista.IntegrationTest
+{
+    public class CardApiTests : IClassFixture<WebApplicationFactory<Api.Startup>>
+    {
+        private readonly WebApplicationFactory<Api.Startup> _factory;
+
+        public CardApiTests(WebApplicationFactory<Api.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task When_Creating_New_Card_Should_Return_Success()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var request = new CreateCardRequest();
+            request.CardHolder = "Teste";
+            request.ExpirationDate = DateTime.Now;
+            request.Number = "784789407238904742389";
+
+            // Act
+
+            var response = await client.PostAsJsonAsync("api/Cards", request);
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ src/Ametista.IntegrationTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Ametista.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Ametista.IntegrationTest
+{
+    public class CustomWebApplicationFactory<TStartup>: WebApplicationFactory<Api.Startup>
+    {
+        public CustomWebApplicationFactory()
+        {
+
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<WriteDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<WriteDbContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    // Ensure the database is created.
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
