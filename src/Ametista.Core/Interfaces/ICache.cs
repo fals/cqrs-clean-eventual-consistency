6a48d8eeffed8261f5c750885f365089d411ac1a
Filipe Lima <fals.xp@gmail.com>
2018-09-20 18:04:57 +0200

    adding cache interface

---
+++ src/Ametista.Core/Interfaces/ICache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ametista.Core.Interfaces
+{
+    public interface ICache
+    {
+        Task Store<T>(string key, T value);
+        Task Get<T>(string key);
+        Task Delete(string key);
+    }
+}
